0ec51bbd551af1cf26ec370b5c0644e084bf0faa
amrendrayd <105165016+amrendrayd@users.noreply.github.com>
2024-03-27 19:54:55 +0530

    Update Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Main from './Main';
+import './Home.scss'
 
+function Home() {
+  return (
+    <div className='Home'>
+        <Navbar/>
+        <Main/> 
+    </div>
+  )
+}
+
+export default Home
